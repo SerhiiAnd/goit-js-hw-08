9f41cd2b2f17e759086a0b6acb49e45089ad58d8
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2023-10-22 18:46:01 +0200

    save-fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -15,43 +15,6 @@ player.ready().then(() => {
       const currentTime = data.seconds;
 
       localStorage.setItem('videoplayer-current-time', currentTime.toString());
-    }, 800)
+    }, 1000)
   );
 });
-
-// const TIME_KEY = 'videoplayer-current-time';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe, {
-//   loop: true,
-//   fullscreen: true,
-//   quality: '1080p',
-// });
-
-// const getCurrentTime = function (currentTime) {
-//   const seconds = currentTime.seconds;
-//   localStorage.setItem(TIME_KEY, JSON.stringify(seconds));
-// };
-
-// player.on('timeupdate', throttle(getCurrentTime, 1000));
-
-// player.setCurrentTime(JSON.parse(localStorage.getItem(TIME_KEY)) || 0);
-
-// const player = new Player(document.getElementById('vimeo-player'));
-
-// const savedTime = localStorage.getItem('videoplayer-current-time');
-
-// player.ready().then(function () {
-//   if (savedTime) {
-//     player.setCurrentTime(parseFloat(savedTime));
-//   }
-
-//   player.on(
-//     'timeupdate',
-//     throttle(function (data) {
-//       const currentTime = data.seconds;
-
-//       localStorage.setItem('videoplayer-current-time', currentTime.toString());
-//     }, 1000)
-//   );
-// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,42 +3,47 @@ import throttle from 'lodash.throttle';
 const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = feedbackForm.querySelector('input[name="email"]');
 const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
+const submitButton = feedbackForm.querySelector('button[type="submit"]');
 
 const localStorageKey = 'feedback-form-state';
 
-// Відстежуємо подію input на формі і зберігаємо дані у локальне сховище з використанням lodash.throttle
-feedbackForm.addEventListener(
-  'input',
-  throttle(() => {
-    const formData = {
-      email: emailInput.value,
-      message: messageTextarea.value,
-    };
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-  }, 500)
-); // Збереження не частіше, ніж раз на 500 мілісекунд
+function saveFormData() {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}
 
-// Перевіряємо сховище при завантаженні сторінки і заповнюємо поля форми
-window.addEventListener('load', () => {
-  const savedFormData = localStorage.getItem(localStorageKey);
-  if (savedFormData) {
-    const formData = JSON.parse(savedFormData);
+function loadFormData() {
+  const savedData = localStorage.getItem(localStorageKey);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
     emailInput.value = formData.email;
     messageTextarea.value = formData.message;
   }
+}
+
+feedbackForm.addEventListener(
+  'input',
+  throttle(() => {
+    saveFormData();
+  }, 500)
+);
+window.addEventListener('load', () => {
+  loadFormData();
 });
 
-// Обробляємо подію сабміту форми
 feedbackForm.addEventListener('submit', event => {
   event.preventDefault();
-
-  // Очищаємо сховище
   localStorage.removeItem(localStorageKey);
 
-  // Виводимо дані у консоль
   const formData = {
     email: emailInput.value,
     message: messageTextarea.value,
   };
   console.log(formData);
+
+  emailInput.value = '';
+  messageTextarea.value = '';
 });
